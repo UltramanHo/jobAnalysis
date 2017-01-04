9072a1fc78564896494abd3cd9f343d0a4d1ea36
Leo King <244494600@qq.com>
2017-01-04 22:10:06 +0800

    提交完整的系统框架。mybatis映射还没有加进去

---
+++ src/main/java/zju/jobs/domain/Position.java
@@ -0,0 +1,106 @@
+package zju.jobs.domain;
+
+public class Position {
+
+	private String companyId;
+	private String companyFullName;
+	private String companyShortName;
+	private String companySize;
+	private String positionName;
+	private String salary;
+	private String workYear;
+	private String secondType;
+	private String industryField;
+	private String financeStage;
+	private String education;
+	private String city;
+	private Float minSalary;
+	private Float maxSalary;
+	
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getCompanyFullName() {
+		return companyFullName;
+	}
+	public void setCompanyFullName(String companyFullName) {
+		this.companyFullName = companyFullName;
+	}
+	public String getCompanyShortName() {
+		return companyShortName;
+	}
+	public void setCompanyShortName(String companyShortName) {
+		this.companyShortName = companyShortName;
+	}
+	public String getCompanySize() {
+		return companySize;
+	}
+	public void setCompanySize(String companySize) {
+		this.companySize = companySize;
+	}
+	public String getPositionName() {
+		return positionName;
+	}
+	public void setPositionName(String positionName) {
+		this.positionName = positionName;
+	}
+	public String getSalary() {
+		return salary;
+	}
+	public void setSalary(String salary) {
+		this.salary = salary;
+	}
+	public String getWorkYear() {
+		return workYear;
+	}
+	public void setWorkYear(String workYear) {
+		this.workYear = workYear;
+	}
+	public String getSecondType() {
+		return secondType;
+	}
+	public void setSecondType(String secondType) {
+		this.secondType = secondType;
+	}
+	public String getIndustryField() {
+		return industryField;
+	}
+	public void setIndustryField(String industryField) {
+		this.industryField = industryField;
+	}
+	public String getFinanceStage() {
+		return financeStage;
+	}
+	public void setFinanceStage(String financeStage) {
+		this.financeStage = financeStage;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public Float getMinSalary() {
+		return minSalary;
+	}
+	public void setMinSalary(Float minSalary) {
+		this.minSalary = minSalary;
+	}
+	public Float getMaxSalary() {
+		return maxSalary;
+	}
+	public void setMaxSalary(Float maxSalary) {
+		this.maxSalary = maxSalary;
+	}
+	
+	
+}
